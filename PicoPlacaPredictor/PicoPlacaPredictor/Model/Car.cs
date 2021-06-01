44d46fc7eaecc32ae9a91bfc2e46fbaf7f935496
ian0000 <niklas0617@gmail.com>
2021-06-01 13:49:18 -0500

    start of proyect, mvc files created, car model created

---
+++ PicoPlacaPredictor/PicoPlacaPredictor/Model/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoPlacaPredictor.Model
+{
+    class Car
+    {
+        //variables 
+
+        int plateNumber { get; set; }
+        string date { get; set; }
+        string time { get; set; }
+
+        //constructors
+        public Car()
+        {
+        }
+
+        public Car(int plateNumber, string date, string time)
+        {
+            this.plateNumber = plateNumber;
+            this.date = date;
+            this.time = time;
+        }
+    }
+}
