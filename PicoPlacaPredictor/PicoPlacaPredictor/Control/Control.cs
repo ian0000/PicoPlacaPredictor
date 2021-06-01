b8dacecfeee69a66fa78f808db094541ee834aef
ian0000 <niklas0617@gmail.com>
2021-06-01 16:09:45 -0500

    Method to identify which car plate number can be in the road depending in the day of the week

--- PicoPlacaPredictor/PicoPlacaPredictor/Control/Control.cs
+++ PicoPlacaPredictor/PicoPlacaPredictor/Control/Control.cs
@@ -20,14 +20,74 @@ namespace PicoPlacaPredictor.Control
             //splits the date when it finds '/'
             string[] splitedDate = date.Split('/');
 
-            //converts the array into a datetime to get a datetime and see which date of the week it is
+            //converts the array into a datetime to get a datetime and see which date of the week it is//formate for datetime AAAA,mm,dd
             DateTime dateTime = new DateTime(int.Parse(splitedDate[2]), int.Parse(splitedDate[1]), int.Parse(splitedDate[0]));
            
             //gets the day of the week//sun = 0,mon=1, tues = 2, wed = 3, thurs = 4, fri = 5, sat = 6
-            int s = (int)dateTime.DayOfWeek;
-            return s;
+            int dayOfWeek = (int)dateTime.DayOfWeek;
+            return dayOfWeek;
         }
         
+        //as there are 5  option i should control with a switch the different options
+        public bool ValidCarDay(int plate, int dayOfWeek)
+        {
+            //if the car can circulate its true//else false?
+            bool resultado = true;
+            switch (dayOfWeek)
+            {
+
+                //0 and 6 aren't necessary but i will let them just to control
+
+                //the license plate number not allowed this days are 1 ^ 2
+                case 1:
+                    if(plate == 1 || plate == 2)
+                    {
+                        resultado = false;
+                    }
+                    break;
+
+                //the license plate number not allowed this days are 3 ^ 4
+                case 2:
+                    if (plate == 3 || plate == 4)
+                    {
+                        resultado = false;
+                    }
+                    break;
+
+
+                //the license plate number not allowed this days are 5 ^ 6
+                case 3:
+                    if (plate == 5 || plate == 6)
+                    {
+                        resultado = false;
+                    }
+                    break;
+
+
+                //the license plate number not allowed this days are 7 ^ 8
+                case 4:
+                    if (plate == 7 || plate == 8)
+                    {
+                        resultado = false;
+                    }
+                    break;
+
+
+                //the license plate number not allowed this days are 9 ^ 0 
+                case 5:
+                    if (plate == 9 || plate == 0)
+                    {
+                        resultado = false;
+                    }
+                    break;
+
+                //default case..i guess this is where 0 and 6 should go?
+                default:
+                    resultado = true;
+                    break;
+            }
+            return resultado;
+        }
 
         //check for time boundaries // 7:00 to 9:30//16:00 to 19:30// should i add an option for am/pm?//verify if its in the time range, like 23:59 cant be 24:61
         public bool TimeValidation(string time)
