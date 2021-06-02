d1c90574ce9155c8ab6c2ed0a46129a8ba3e1790
ian0000 <niklas0617@gmail.com>
2021-06-02 14:05:02 -0500

    2 Automated test cases for dayofweek method created

---
+++ PicoPlacaPredictor/PicoPlacaTest.UnitTest/ControlTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PicoPlacaPredictor;
+using PicoPlacaPredictor.Control;
+using System;
+
+namespace PicoPlacaTest.UnitTest
+{
+    [TestClass]
+    public class ControlTests
+    {
+        //test in case the values of date is out of a normal date range
+        [TestMethod]
+        public void DayOfWeek_DateOffRange_Returns7()
+        {
+            //arrange
+            var control = new Control();
+
+            //act
+            var result = control.DayOfWeek("35/13/2021");
+
+            //assert
+            Assert.AreEqual(result, 7);
+        }
+
+        [TestMethod]
+        public void DayOfWeek_DateCorrect_ReturnsLessThan7()
+        {
+            //arrange
+            var control = new Control();
+
+            //act
+            var result = control.DayOfWeek("24/01/2021");
+
+            //assert
+            bool solution = false;
+            if (result < 7)
+            {
+                solution = true;
+            }
+            Assert.IsTrue(solution);
+        }
+    }
+}
---
+++ PicoPlacaPredictor/PicoPlacaTest.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PicoPlacaTest.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PicoPlacaTest.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e7491a1-5612-4f68-835c-0ae2bce557dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
